faa8c7a816e53bac47dfeb1f68d7087fb8d37574
Noxikhai <noxikhai@gmail.com>
2022-09-10 03:01:28 +0800

    added exit game function in exit button

---
+++ CapstoneProject/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public Image confirmPanelBackground;
+
+
+
+    public void QuitApplication()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+
+
+    }
+
+
+}
