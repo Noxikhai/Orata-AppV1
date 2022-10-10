a4ae376f07ca7ff3b4766249cca6ad4ef060753d
Noxikhai <noxikhai@gmail.com>
2022-10-11 03:17:31 +0800

    update controller.cs
    change overlapcircle from 0.2f to 0.1f

--- CapstoneProject/Assets/Scripts/Player/PlayerController.cs
+++ CapstoneProject/Assets/Scripts/Player/PlayerController.cs
@@ -53,7 +53,7 @@ public class PlayerController : MonoBehaviour
 
     private bool IsWalkable(Vector3 targetPos)
     {
-        if(Physics2D.OverlapCircle(targetPos, 0.2f, solidObjectsLayer) != null)
+        if(Physics2D.OverlapCircle(targetPos, 0.1f, solidObjectsLayer) != null)
         {
             return false;
         }
